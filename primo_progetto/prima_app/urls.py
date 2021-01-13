dde9e498de4853ed002c30fce4351812a4127ab2
Pygeddon <Java1981@>
2021-01-13 02:42:42 +0100

    prima app creazione urls e view

--- primo_progetto/prima_app/urls.py
+++ primo_progetto/prima_app/urls.py
@@ -13,13 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
+
+from django.urls import path
 from prima_app import views as prima_app_views
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    #path('', prima_app_views.homepage, name='home')
-    path('prima_app/', include('prima_app.urls'))
+    path('', prima_app_views.homepage, name='home')
+
 ]
